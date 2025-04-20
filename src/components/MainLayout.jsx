808c975a3419852c420f22e79afc5787a08d77f5
AnuGee <wanuwat@hotmail.com>
2025-04-20 12:00:04 +0700

    Update MainLayout.jsx

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -4,30 +4,28 @@ import React from "react";
 import Header from "./Header";
 
 export default function MainLayout({ children }) {
-  return (
-    <>
-      <Header />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          width: "100%",
-          backgroundColor: "#f9fafb",
-          minHeight: "100vh",
-        }}
-      >
+    return (
         <div
-          style={{
-            maxWidth: "1200px",
-            width: "100%",
-            padding: "0 2rem",
-            margin: "0 auto",
-            boxSizing: "border-box",
-          }}
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                width: "100%",
+                backgroundColor: "#f9fafb",
+                minHeight: "100vh",
+            }}
         >
-          <div style={{ marginTop: "1rem" }}>{children}</div>
+            <div
+                style={{
+                    maxWidth: "1200px",
+                    width: "100%",
+                    padding: "0 2rem",
+                    margin: "0 auto",
+                    boxSizing: "border-box",
+                }}
+            >
+                <Header /> {/* ย้าย Header เข้ามาใน div นี้ */}
+                <div style={{ marginTop: "1rem" }}>{children}</div>
+            </div>
         </div>
-      </div>
-    </>
-  );
+    );
 }
