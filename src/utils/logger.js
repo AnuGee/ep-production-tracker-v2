4b780b848e0c70f12a85bd224980b70ed991924a
AnuGee <wanuwat@hotmail.com>
2025-06-09 16:03:31 +0700

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -4,11 +4,15 @@ import { db } from "../firebase";
 
 export async function logEvent({ email, action, page, metadata = {} }) {
   try {
+    const userAgent = navigator.userAgent;
+    const platform = navigator.platform;
+
     await addDoc(collection(db, "user_activity_logs"), {
       email,
       action,
       page,
       metadata,
+      user_agent: `${platform} - ${userAgent}`,
       timestamp: serverTimestamp(),
     });
   } catch (error) {
